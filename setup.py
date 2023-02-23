7332b8e727de182ff8bb9646d18b54c4cee38414
RichieHakim <RichHakim@gmail.com>
2023-02-23 15:43:08 -0500

    bug typo

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Richard Hakim',
     packages=setuptools.find_packages(),
     install_requires=['torch>=1.7'],
-    packages=['rp'],
+    # packages=['rp'],
     long_description=open('README.md').read(),
     url='https://github.com/RichieHakim/rolling_percentile',
 )
